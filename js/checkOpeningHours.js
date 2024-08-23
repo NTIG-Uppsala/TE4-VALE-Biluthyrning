fd8043930fc14e8822596f4befd94e4b3ab110d8
viggoStrom <viggostrom05@gmail.com>
2024-08-23 15:58:36 +0200

    dynamic opening hours

--- js/checkOpeningHours.js
+++ js/checkOpeningHours.js
@@ -6,7 +6,6 @@ dropdownArrow.setAttribute("data-state", "closed");
 dropdownArrow.addEventListener("click", () => {
     dropdown.classList.toggle("open-dropdown");
 
-    console.log(dropdownArrow.getAttribute("data-state"));
     if (dropdownArrow.getAttribute("data-state") === "closed") {
         dropdownArrow.setAttribute("data-state", "open");
         dropdownArrow.style.transform = "rotate(180deg)";
@@ -24,7 +23,7 @@ Object.keys(openHours).forEach(key => {
     let dayString = ""
 
     dayString += day.name + " "
-    if(day.from || day.to) {
+    if (day.from || day.to) {
         dayString += day.from.slice(0, 2) + ":" + day.from.slice(2);
         dayString += " - ";
         dayString += day.to.slice(0, 2) + ":" + day.to.slice(2);
@@ -46,3 +45,18 @@ document.querySelectorAll(".insert-open-hours-after").forEach(element => {
 const openStatusTag = document.querySelector("p.open-status");
 
 const now = new Date();
+
+Object.keys(openHours).forEach(key => {
+    if (key == now.getDay()) {
+        if (closedDates[now.getMonth()] && closedDates[now.getMonth()][now.getDate()]) {
+            openStatusTag.innerHTML = "Vi har tyvärr stängt idag på grund av: " + closedDates[now.getMonth()][now.getDate()] + ".";
+        }
+        else if (!openHours[key].from || !openHours[key].to) {
+            openStatusTag.innerHTML = "Vi har tyvärr stängt idag.";
+        } else {
+            openStatusTag.innerHTML = `Vi har öppet idag, ${openHours[key].nameSingular} ${openHours[key].from.slice(0, 2) + ":" + openHours[key].from.slice(2)} - ${openHours[key].to.slice(0, 2) + ":" + openHours[key].to.slice(2)}.`;
+        }
+    }
+});
+
+// tell people next time they open if they're closed
--- js/openHours.js
+++ js/openHours.js
@@ -1,10 +1,17 @@
 
 const openHours = {
-    monday: { name: "Måndagar", from: "1000", to: "1600" },
-    tuesday: { name: "Tisdagar", from: "1000", to: "1600" },
-    wednesday: { name: "Onsdagar", from: "1000", to: "1600" },
-    thursday: { name: "Torsdagar", from: "1000", to: "1600" },
-    friday: { name: "Fredagar", from: "1000", to: "1600" },
-    saturday: { name: "Lördagar", from: "1200", to: "1500" },    
-    sunday: { name: "Söndagar", from: false, to: false }
+    1: { name: "Måndagar", nameSingular: "måndag", from: "1000", to: "1600" },
+    2: { name: "Tisdagar", nameSingular: "tisdag", from: "1000", to: "1600" },
+    3: { name: "Onsdagar", nameSingular: "onsdag", from: "1000", to: "1600" },
+    4: { name: "Torsdagar", nameSingular: "torsdag", from: "1000", to: "1600" },
+    5: { name: "Fredagar", nameSingular: "fredag", from: "1000", to: "1600" },
+    6: { name: "Lördagar", nameSingular: "lördag", from: "1200", to: "1500" },
+    0: { name: "Söndagar", nameSingular: "söndag", from: false, to: false }
 };
+
+const closedDates = {
+    0: { 1: "Nyårsdagen", 6: "Trettondagen" },
+    4: { 1: "Första maj" },
+    5: { 6: "Nationaldagen" },
+    11: { 24: "Julafton", 25: "Juldagen", 26: "Annandag jul", 31: "Nyårsafton" }
+}
