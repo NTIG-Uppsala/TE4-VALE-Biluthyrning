6081112bdedcbaf70e8760a5197c0822c5896216
viggoStrom <viggostrom05@gmail.com>
2024-08-28 13:31:59 +0200

    small letters on the months and singular forms of day of the week

--- js/openHours.js
+++ js/openHours.js
@@ -14,13 +14,13 @@
  * @type {OpenHours}
  */
 const openHours = {
-    1: { order: 0, name: "Måndagar", nameSingular: "måndag", from: "1000", to: "1600" },
-    2: { order: 1, name: "Tisdagar", nameSingular: "tisdag", from: "1000", to: "1600" },
-    3: { order: 2, name: "Onsdagar", nameSingular: "onsdag", from: "1000", to: "1600" },
-    4: { order: 3, name: "Torsdagar", nameSingular: "torsdag", from: "1000", to: "1600" },
-    5: { order: 4, name: "Fredagar", nameSingular: "fredag", from: "1000", to: "1600" },
-    6: { order: 5, name: "Lördagar", nameSingular: "lördag", from: "1200", to: "1500" },
-    0: { order: 6, name: "Söndagar", nameSingular: "söndag", from: false, to: false }
+    1: { order: 0, name: "Måndag", nameSingular: "måndag", from: "1000", to: "1600" },
+    2: { order: 1, name: "Tisdag", nameSingular: "tisdag", from: "1000", to: "1600" },
+    3: { order: 2, name: "Onsdag", nameSingular: "onsdag", from: "1000", to: "1600" },
+    4: { order: 3, name: "Torsdag", nameSingular: "torsdag", from: "1000", to: "1600" },
+    5: { order: 4, name: "Fredag", nameSingular: "fredag", from: "1000", to: "1600" },
+    6: { order: 5, name: "Lördag", nameSingular: "lördag", from: "1200", to: "1500" },
+    0: { order: 6, name: "Söndag", nameSingular: "söndag", from: false, to: false }
 };
 
 /**
@@ -29,7 +29,7 @@ const openHours = {
  */
 
 const closedDates = {
-    0: { 1: "Nyårsdagen", 6: "Trettondagen" },
+    0: { 1: "Nyårsdagen", 6: "Trettondedag Jul" },
     4: { 1: "Första maj" },
     5: { 6: "Nationaldagen" },
     11: { 24: "Julafton", 25: "Juldagen", 26: "Annandag jul", 31: "Nyårsafton" }
