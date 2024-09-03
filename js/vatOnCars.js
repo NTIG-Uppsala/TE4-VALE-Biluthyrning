985a138e1635532af0060179249aef91a38e9d5d
viggoStrom <viggostrom05@gmail.com>
2024-09-03 13:14:03 +0200

    patched double click VAT

--- js/vatOnCars.js
+++ js/vatOnCars.js
@@ -1,5 +1,5 @@
-const buttonContainer = document.querySelector(
-    ".our-cars-section>.vat-container"
+const buttons = document.querySelectorAll(
+    ".our-cars-section>.vat-container>button"
 );
 
 const vat = 1.25;
@@ -7,7 +7,6 @@ const vat = 1.25;
 const prices = Array.from(
     document.querySelectorAll(".our-cars-section .price")
 );
-
 const pricesVatIncluded = prices.map(
     (price) => parseInt(price.textContent).toFixed(0) + " kr"
 );
@@ -15,21 +14,54 @@ const pricesVatExcluded = prices.map(
     (price) => (parseInt(price.textContent) / vat).toFixed(0) + " kr"
 );
 
+const setVatIncluded = () => {
+    prices.forEach((price, index) => {
+        price.textContent = pricesVatIncluded[index];
+    });
+}
+const setVatExcluded = () => {
+    prices.forEach((price, index) => {
+        price.textContent = pricesVatExcluded[index];
+    });
+}
 
-Array.from(buttonContainer.children).forEach((button, index) => {
-    button.addEventListener("click", () => {
-        if (buttonContainer.children[1].classList.contains("active")) {
-            prices.forEach((price, index) => {
-                price.textContent = pricesVatIncluded[index];
-            });
+buttons.forEach((button, index) => {
+    button.classList.add("button" + index);
+
+    button.addEventListener("click", (event) => {
+        if (event.target.classList.contains("active")) {
+            return;
+        };
+
+        buttons.forEach((button) => button.classList.remove("active"));
+
+        event.target.classList.add("active");
+
+        if (button.classList.contains("button0")) {
+            setVatIncluded();
         } else {
-            prices.forEach((price, index) => {
-                price.textContent = pricesVatExcluded[index];
-            });
-        }
-        Array.from(buttonContainer.children).forEach((button) =>
-            button.classList.remove("active")
-        );
-        button.classList.add("active");
+            setVatExcluded();
+        };
     });
 });
+
+// Array.from(buttonContainer.children).forEach((button, index) => {
+//     button.addEventListener("click", () => {
+
+
+
+//         if (buttonContainer.children[1].classList.contains("active")) {
+//             prices.forEach((price, index) => {
+//                 price.textContent = pricesVatIncluded[index];
+//             });
+//         } else {
+//             prices.forEach((price, index) => {
+//                 price.textContent = pricesVatExcluded[index];
+//             });
+//         }
+//         Array.from(buttonContainer.children).forEach((button) =>
+//             button.classList.remove("active")
+//         );
+//         button.classList.add("active");
+//     });
+// });
