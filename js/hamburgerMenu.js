10774328b9bb11b3e9772b2c5ced8da63125d124
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2024-09-06 14:43:17 +0200

    Hamburger menu implementation

---
+++ js/hamburgerMenu.js
@@ -0,0 +1,24 @@
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+        // Get the dropdown menu
+        const dropdownMenu = document.querySelector(".dropdown-menu");
+
+        // Toggle the display of the dropdown menu
+        dropdownMenu.style.display =
+            dropdownMenu.style.display === "flex" ? "none" : "flex";
+    });
+
+document.addEventListener("click", (event) => {
+    // Get the dropdown menu
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+    // If the dropdown menu is clicked
+    if (event.target === hamburgerMenu) {
+        // Do nothing
+        return;
+    }
+
+    // If the dropdown menu is not clicked
+    // Hide the dropdown menu
+    dropdownMenu.style.display = "none";
+});
