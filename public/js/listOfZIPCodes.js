11f2c8863cbb52f7835dad980d9d8321f3070784
ivar117 <ivar.bjerling@elev.ga.ntig.se>
2024-09-12 13:35:00 +0200

    removed 98145 as a valid zip code

--- public/js/listOfZIPCodes.js
+++ public/js/listOfZIPCodes.js
@@ -4,7 +4,6 @@ const zipCodes = [
     { zipCode: "98140", price: "199" },
     { zipCode: "98141", price: "199" },
     { zipCode: "98144", price: "299" },
-    { zipCode: "98145", price: "299" },
     { zipCode: "98146", price: "299" },
     { zipCode: "98147", price: "299" },
 ];
