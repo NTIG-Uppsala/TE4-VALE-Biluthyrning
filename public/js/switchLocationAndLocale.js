14eb6c1c6538be15c410e863b6f522ad38f09665
ivar117 <ivar.bjerling@elev.ga.ntig.se>
2024-09-17 15:00:54 +0200

    Working dropdown for locale select
    Added new HTML structure, new CSS, flag icons instead of emojis, updated switchLocationAndLocale.js accordingly, added toggleDropdown.js

--- public/js/switchLocationAndLocale.js
+++ public/js/switchLocationAndLocale.js
@@ -1,15 +1,42 @@
+const locationSelect = document.querySelector(".redirect-dropdowns #location-select");
+const localeSelect = document.getElementById("redirect-dropdown-content");
 
-const locationSelect = document.querySelector(".select-container #location-select");
-const localeSelect = document.querySelector(".select-container #language-select");
+// This will update the location when a new location is chosen
+if (locationSelect) {
+    locationSelect.addEventListener("change", () => {
+        const location = locationSelect.value;
+        window.location = "../" + location;
+    });
+}
 
-locationSelect.addEventListener("change", () => {
-    const location = locationSelect.value;
-    window.location = "../" + location;
+// Switch locale on language option click
+const languageContainers = localeSelect.querySelectorAll(".language-container");
+languageContainers.forEach(container => {
+    container.addEventListener("click", () => {
+        // Get the selected value from the data-value attribute
+        const locale = container.getAttribute('data-value');
+        
+        // Handle exceptions for the country code
+        let countryCode = "";
+        if (locale === "en") {
+            countryCode = "gb";
+        }
+        else {
+            countryCode = locale;
+        }
+
+        // Give the user confirmation that the language has been changed before refreshing
+        const dropDownHead = document.getElementById("redirect-dropdown-head");
+        dropDownHead.innerHTML = `
+            <span class="fi fi-${countryCode}"></span> <!--Flag icon. The country is specified through fi-xx, where xx is replaced with the country code.-->
+            <span class="language-name">${container.querySelector('.language-name').textContent}</span> 
+			<img src="../../assets/icons/icons8-expand-arrow-48.png" id="arrow" alt="down arrow" width="18">
+
+        `;
+
+        // Redirect based on current location
+        const windowPath = window.location.pathname.split("/");
+        const location = windowPath[windowPath.length - 2] || "kiruna"; // Default location if not found
+        window.location = window.location.pathname.replace("index.html", "") + "../../" + locale + "/" + location;
+    });
 });
-
-localeSelect.addEventListener("change", () => {
-    const windowPath = window.location.pathname.split("/");
-    const location = windowPath[windowPath.length - 2] || "kiruna";
-    const locale = localeSelect.value;
-    window.location = window.location.pathname.replace("index.html", "") + "../../" + locale + "/" + location;
-}); 
